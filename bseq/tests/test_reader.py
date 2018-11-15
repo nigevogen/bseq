101d3795b9a208966b5385b876eb9cbd67e006cc
kent <kentkawashima@gmail.com>
2018-11-15 19:39:42 +0900

    Tests read_fasta_alignment

--- bseq/tests/test_reader.py
+++ bseq/tests/test_reader.py
@@ -2,7 +2,7 @@
 """Nose tests for reader functions.
 """
 import tempfile
-from bseq.reader import read_fasta_file
+from bseq.reader import read_fasta_file, read_fasta_alignment
 
 class TestReadFastaFile:
     """Unit tests for read_fasta_file
@@ -27,4 +27,26 @@ class TestReadFastaFile:
         seq_list = read_fasta_file(self.path)
         assert len(seq_list) == 2
 
+class TestReadFastaAlignment:
+    """Unit tests for read_fasta_file
+    """
+    def setup(self):
+        self.fp = tempfile.NamedTemporaryFile(mode='w+')
+        lines = ['>Test1 This is a description',
+                 'ATGCATGCATGCAAA',
+                 'ATGCATGCATGCAAA',
+                 '>Test2 This is a another description',
+                 'CATGCATGCAAATTT',
+                 'CATGCATGCAAATTT',
+                 '']
+        self.fp.write('\n'.join(lines))
+        self.fp.seek(0)
+        self.path = self.fp.name
+
+    def teardown(self):
+        self.fp.close()
+
+    def test_read_fasta_alignment(self):
+        seq_list = read_fasta_alignment(self.path)
+        assert len(seq_list) == 30
 
