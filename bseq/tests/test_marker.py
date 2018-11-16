d9795c2e0b4d1c38ae39de2285ae1ecfbda27cba
kent <kentkawashima@gmail.com>
2018-11-16 12:08:07 +0900

    Adds test for Marker._encode

--- bseq/tests/test_marker.py
+++ bseq/tests/test_marker.py
@@ -15,6 +15,13 @@ class TestMarker:
     def test_encoded_sequence(self):
         assert self.marker.encoded_sequence == '0O3X4O10X13O15'
 
+    def test_encode(self):
+        self.marker._encode('XOOXOOOOOOXXXOX')
+        assert self.marker._char_list == ('X', 'O', 'X', 'O', 'X', 'O', 'X')
+        assert self.marker._pos_list == (
+            (0, 1), (1, 3), (3, 4), (4, 10), (10, 13), (13, 14), (14, 15)
+        )
+
     def test_coords(self):
         assert self.marker.coords('X') == [3, 10, 11, 12]
         assert self.marker.coords('O') == [0, 1, 2, 4, 5, 6, 7, 8, 9, 13, 14]
